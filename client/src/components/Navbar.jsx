68ea739d849c423934c5ee3891f6a33adfe288be
Anurag Kaushik <anurag11b12345@gmail.com>
2025-02-03 19:55:37 +0530

    frontend updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,34 +1,56 @@
-import React from 'react';
 import { assets } from '../assets/assets';
 import { useNavigate } from 'react-router-dom';
 import { AppContent } from '../context/AppContext';
 import { useContext } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Navbar = () => {
-
   const navigate = useNavigate();
-  const {userData, backendUrl, setIsLoggedin,setUserData } = useContext(AppContent);
+  const { userData, backendUrl, setIsLoggedin, setUserData } = useContext(AppContent);
+
+  const logout = async () => {
+    try {
+      axios.defaults.withCredentials = true;
+      const { data } = await axios.post(`${backendUrl}/auth/logout`);
+      if (data.success) {
+        setIsLoggedin(false);
+        setUserData(null);
+        navigate('/');
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
 
   return (
     <div className='w-full flex justify-between items-center p-4 sm:px-24 absolute top-0'>
-        <img src={assets.fixly} alt='' className='w-28 sm:w-32' />
-        { userData ? 
-        <div className='w-8 h-8 bg-black text-white rounded-full flex items-center justify-center cursor-pointe relative group'>
-          {userData.name[0].toUpperCase()} 
-          <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10 '>
+      <img src={assets.fixly} alt='' className='w-28 sm:w-32' />
+      {userData ? (
+        <div className='w-8 h-8 bg-black text-white rounded-full flex items-center justify-center cursor-pointer relative group'>
+          {userData.name[0].toUpperCase()}
+          <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10'>
             <ul className='list-none m-0 p-2 bg-gray-100 text-sm'>
-              {!userData.isAccountVerified && <li className='cursor-pointer py-1 pb-2.5 px-2 hover:bg-gray-200'>Verify Email</li>}
-              
-              <li className='cursor-pointer py-1 px-2 hover:bg-gray-200 pr-10'>Logout</li>
+              {!userData.isAccountVerified && (
+                <li className='cursor-pointer py-1 pb-2.5 px-2 hover:bg-gray-200'>Verify Email</li>
+              )}
+              <li onClick={logout} className='cursor-pointer py-1 px-2 hover:bg-gray-200 pr-10'>
+                Logout
+              </li>
             </ul>
           </div>
-        </div>:
-        <button 
-        onClick={() => navigate('/login')}
-        className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all cursor-pointer '>Login</button>
-      }
+        </div>
+      ) : (
+        <button
+          onClick={() => navigate('/login')}
+          className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all cursor-pointer'
+        >
+          Login
+        </button>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
